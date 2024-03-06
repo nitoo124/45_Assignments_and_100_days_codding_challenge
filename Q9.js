8b920d102fdb95f5aae3cd9a7f8600a8714a858c
nitoo124 <160069703+nitoo124@users.noreply.github.com>
2024-03-06 05:59:48 -0800

    Add files via upload

---
+++ Q9.js
@@ -0,0 +1,5 @@
+/*Favorite Number: Store your favorite number in a variable.
+Then, using that variable, create a message that reveals your favorite number.
+ Print that message.*/
+var fav_num = 7;
+console.log(fav_num + ' is my favorite number.');
---
+++ Q9.ts
@@ -0,0 +1,7 @@
+/*Favorite Number: Store your favorite number in a variable. 
+Then, using that variable, create a message that reveals your favorite number.
+ Print that message.*/
+
+ let fav_num : number = 7;
+
+ console.log(fav_num + ' is my favorite number.');

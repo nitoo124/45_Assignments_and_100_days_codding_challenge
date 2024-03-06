dd855874afd351c9834e97f6b5934b42f1a493e9
nitoo124 <160069703+nitoo124@users.noreply.github.com>
2024-03-06 12:44:10 -0800

    Add files via upload

---
+++ Q10.ts
@@ -0,0 +1,22 @@
+/*Adding Comments: Choose two of the programs you’ve written,and add at least one comment to each. 
+ If you don’t have anything specific to write because your programs are too simple at this point, 
+ just add your name and the current date at the top of each program file.
+  Then write one sentence describing what the program does.*/
+
+
+//
+  // here i printed my favorite number
+  let Fav_num : number = 7;
+
+ console.log(fav_num + ' is my favorite number.');
+
+
+ console.log(2 + 6);
+ console.log(12 - 4);
+ console.log(2 * 4);
+ console.log(16 / 2);
+
+
+
+
+
---
+++ Q11.js
@@ -0,0 +1,4 @@
+var names = ['Naina', 'Neha', 'Noor'];
+console.log(names[0]);
+console.log(names[1]);
+console.log(names[2]);

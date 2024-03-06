11ea43599244b80b032cd5aeb660eebe18235882
nitoo124 <160069703+nitoo124@users.noreply.github.com>
2024-03-06 13:24:27 -0800

    Add files via upload

---
+++ Q12.js
@@ -0,0 +1,11 @@
+/*Greetings: Start with the array you used in Exercise 11,
+ but instead of just printing each person’s name, print a message to them.
+  The text of each message should be the same,
+  but each message should be personalized with the person’s name.
+
+*/
+var nameS = ['Naina', 'Neha', 'Noor'];
+var SAME_msg = 'Would you like some Tea?';
+console.log(nameS[0] + ' ' + SAME_msg);
+console.log(nameS[1] + ' ' + SAME_msg);
+console.log(nameS[2] + ' ' + SAME_msg);
---
+++ Q12.ts
@@ -0,0 +1,12 @@
+/*Greetings: Start with the array you used in Exercise 11,
+ but instead of just printing each person’s name, print a message to them.
+  The text of each message should be the same, 
+  but each message should be personalized with the person’s name.
+
+*/
+
+let nameS : string[] = ['Naina', 'Neha', 'Noor'];
+let SAME_msg : string = 'Would you like some Tea?'
+console.log(nameS[0] + ' ' +SAME_msg);
+console.log(nameS[1] + ' ' + SAME_msg);
+console.log(nameS[2] + ' ' +   SAME_msg);

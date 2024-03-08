03e3a0f0ef4302ecd8764451d361c15d28bdeb41
nitoo124 <160069703+nitoo124@users.noreply.github.com>
2024-03-07 16:22:29 -0800

    Add files via upload

---
+++ Q20.js
@@ -0,0 +1,6 @@
+//Q :20
+var countries = ['Pakistan', 'India', 'America', 'Canada', 'UK'];
+console.log('list og countries');
+countries.forEach(function (countries) {
+    console.log(countries);
+});
---
+++ Q20.ts
@@ -0,0 +1,8 @@
+//Q :20
+let countries : string[] = ['Pakistan','India', 'America', 'Canada', 'UK'];
+console.log('list og countries');
+countries.forEach(countries => {
+    console.log(countries);
+    
+}); 
+    

5da22f30055cb5c85c65f56aa59c23451285ef3a
nitoo124 <160069703+nitoo124@users.noreply.github.com>
2024-03-07 16:06:36 -0800

    Add files via upload

---
+++ Q19.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var Q15_1 = require("./Q15");
+for (var i = 0; i < Q15_1.guest.length; i++) {
+    console.log("you are invited to dinner dear ".concat(Q15_1.guest[i]));
+}
---
+++ Q19.ts
@@ -0,0 +1,5 @@
+import { guest } from "./Q15";
+for (let i = 0; i < guest.length; i++) {
+    console.log(`you are invited to dinner dear ${guest[i]}`);
+
+}

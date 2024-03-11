3a90f2dd96d0636e6c795c27c7cdd3c062051125
Nitoo kumari <160069703+nitoo124@users.noreply.github.com>
2024-03-11 14:42:06 -0700

    Add files via upload

---
+++ Q26.js
@@ -0,0 +1,20 @@
+/*Alien Colors #2: Choose a color for an alien as you did in Exercise 25, and write an if-else chain.
+• If the alien’s color is green, print a statement that the player just earned 5 points for shooting the alien.
+
+• If the alien’s color isn’t green, print a statement that the player just earned 10 points.
+
+• Write one version of this program that runs the if block and another that runs the else block.*/
+var Alien_color = 'green';
+if (Alien_color == 'green') {
+    console.log(' player just earned 5 points for shooting the alien.');
+}
+else {
+    console.log(' player just earned 10 points.');
+}
+var Alien_Color = 'red';
+if (Alien_Color == 'green') {
+    console.log(' player just earned 5 points for shooting the alien.');
+}
+else {
+    console.log(' player just earned 10 points.');
+}
